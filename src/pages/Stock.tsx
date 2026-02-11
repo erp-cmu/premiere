dd9a91760194f890510e3f084c627475d14c9746
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-02-11 09:37:16 +0700

    Update

--- src/pages/Stock.tsx
+++ src/pages/Stock.tsx
@@ -1,10 +1,15 @@
 import { useCompany, useItem, useWarehouse } from "@/hooks/siteInfo";
 import { useFrappeCreateDoc } from "frappe-react-sdk";
+import { useState } from "react";
 
 const Doc = () => {
   const { company } = useCompany();
   const { warehouse } = useWarehouse();
   const { items } = useItem();
+  const [qty, setQty] = useState(0);
+  const [itemCode, setItemCode] = useState("");
+
+  console.log({ itemCode, qty, items });
 
   const { createDoc, loading, error, isCompleted, reset } =
     useFrappeCreateDoc();
@@ -17,10 +22,12 @@ const Doc = () => {
       posting_time: new Date().toISOString().split("T")[1].split(".")[0],
       items: [
         {
-          item_code: items && items[0].item_code,
-          item_name: items && items[0].item_name,
+          item_code: itemCode,
+          item_name:
+            items &&
+            items.find((item) => item.item_code === itemCode)?.item_name,
           t_warehouse: warehouse.stores.name,
-          qty: 10,
+          qty: qty,
         },
       ],
     });
@@ -29,12 +36,33 @@ const Doc = () => {
   return (
     <>
       Add Stock Entry
-      <select>
-        {items && items.map((item) => <option>{item.item_name}</option>)}
+      <select onChange={(e) => setItemCode(e.target.value)} value={itemCode}>
+        <option value="" disabled selected>
+          Select Item
+        </option>
+        {items &&
+          items.map((item) => (
+            <option key={item.item_code} value={item.item_code}>
+              {item.item_name}
+            </option>
+          ))}
       </select>
-      <input type="number"></input>
+      <input
+        type="number"
+        value={qty}
+        onChange={(e) => setQty(Number(e.target.value))}
+      ></input>
       <button onClick={handleCreate} className="btn btn-primary">
         Create Stock Entry
+      </button>
+      <button
+        onClick={() => {
+          setQty(0);
+          setItemCode("");
+          reset();
+        }}
+      >
+        Reset
       </button>
     </>
   );
